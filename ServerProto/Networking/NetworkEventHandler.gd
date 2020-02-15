3863ecf40b8bc05f1cbb7aeead806bed6ace83d2
Tdead1 <tomkemna@gmail.com>
2020-02-15 18:27:26 +0100

    UPDATE: Fixing connecting and disconnecting after networkhandler.

--- ServerProto/Networking/NetworkEventHandler.gd
+++ ServerProto/Networking/NetworkEventHandler.gd
@@ -10,35 +10,34 @@ func _ready():
 
 func ConnectPeer(id):	
 	rpc("create_player", id);
+	rpc_id(id, "create_players", ids);
 	return;
 
 func DisconnectPeer(id):
 	rpc("remove_player", id);
 	return;
 
+master func create_player(id):
+	get_parent().debuglog += "Users now online: " + str(get_tree().get_network_connected_peers().size());
+	get_parent().debuglog += "   -> User connected.      ID: " + str(id) + "\n";
+	
+	var newplayer = playertemplate.instance();
+	get_parent().add_child(newplayer);
+	newplayer.SetupOwner(id);
+	
+	ids.append(id);
+	players.append(newplayer);
+	
+	return;
+
 master func remove_player(id):
 	var oldplayer = get_node("/root/Root/Player#" + str(id));
 	oldplayer.queue_free();
+		
 	get_parent().debuglog += "Users now online: " + str(get_tree().get_network_connected_peers().size()) ;
 	get_parent().debuglog += "   -> User disconnected. ID: " + str(id) + "\n";
 	players.erase(oldplayer);
 	ids.erase(id);
-	return;
-	
-master func create_player(id):
-	get_parent().debuglog += "Users now online: " + str(get_tree().get_network_connected_peers().size());
-	get_parent().debuglog += "   -> User connected.    ID: " + str(id) + "\n";
-	
-	var newplayer = playertemplate.instance();
-	add_child(newplayer);
-	newplayer.SetupOwner(id);
-	ids.append(id);
-	players.append(newplayer);
-	get_parent().debuglog += "Calling create player on server.    ID: " + str(id) + "\n";
-	
-	return;
-
-func create_players(ids):
 	return;
 
 func GetDamage(ID, damage):
