a71b7d065be3f6a75270ae93c9b494dcd40f7469
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2020-06-08 20:50:36 +0200

    Fixing gravity on the project, although slopes are still buggy. Making the player pawn not get stuck anymore.

--- ClientProto/Player/Logic/PlayerPawn.gd
+++ ClientProto/Player/Logic/PlayerPawn.gd
@@ -6,17 +6,21 @@ var myMoveInput = Vector3(0,0,0);
 var myHealth = 100.0;
 var myCamera;
 var myNetworkEventHanlder;
+var myCollisionShape : CollisionShape;
+var myVelocity = Vector3(0,0,0);
+
 
 func _ready():
 	set_network_master(get_tree().get_network_unique_id());
 	myCamera = get_node("PlayerCamera");
+	myCollisionShape = get_node("CollisionShape");
 	myNetworkEventHanlder = get_parent().get_node("NetworkEventHandler");
 	return;
 	
 
 func _process(delta):
 	# Input
-	myMoveInput = Vector3(0,0,0);
+	myMoveInput = Vector3(0, 0, 0);
 	if(Input.is_key_pressed(KEY_W)):
 		myMoveInput.x += mySpeed * delta;
 	if(Input.is_key_pressed(KEY_S)):
@@ -28,13 +32,20 @@ func _process(delta):
 	if(Input.is_action_just_pressed("Disconnect")):
 		print("Disconnecting from server.");
 		get_tree().set_network_peer(null);
-	else:
-		myMoveInput.y -= 10.0;
+
 	return;
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	# Gravity
+	if(!test_move(transform, Vector3(0, -0.01, 0))):
+		myMoveInput.y = 0.0 if myVelocity.y > 0 else myVelocity.y - 0.4;
+		
+	print(!test_move(transform, Vector3(0, -0.01, 0)));
+	
 	# Send the server all the information we need!
-	move_and_slide(myMoveInput.rotated(Vector3(0,1,0), get_node("PlayerCamera").rotation.y + 0.5 * PI));
+	myVelocity = move_and_slide(myMoveInput.rotated(Vector3(0,1,0), get_node("PlayerCamera").rotation.y + 0.5 * PI), Vector3(0,1,0));
+		
 	if(myNetworkEventHanlder.myIsConnected):
 		rpc_unreliable_id(1, "UpdatePlayerTransform", transform, myCamera.transform); 
+		
 	return;  
