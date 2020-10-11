74ab2271a3305e3f34b80143f830fd2e490a86d9
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2020-10-11 17:58:07 +0200

    Updating movement system again
    Still need to figure out what to do with upward slopes.

--- ClientProto/Player/Logic/PlayerPawn.gd
+++ ClientProto/Player/Logic/PlayerPawn.gd
@@ -1,13 +1,16 @@
 extends KinematicBody
 
 # class member variables:
-export var mySpeed = 300.0;
+export var myAcceleration = 100;
+export var myMaxSpeed = 15;
+export var myFriction = 3;
 var myMoveInput = Vector3(0,0,0);
 var myHealth = 100.0;
 var myCamera;
 var myNetworkEventHanlder;
 var myCollisionShape : CollisionShape;
 var myVelocity = Vector3(0,0,0);
+var myGravity = 1;
 
 func _ready():
 	set_network_master(get_tree().get_network_unique_id());
@@ -29,45 +32,32 @@ func _process(delta):
 	if(Input.is_key_pressed(KEY_D)):
 		myMoveInput.z += 1;
 	if(myMoveInput.length() > 0):
-		myMoveInput = mySpeed * delta * myMoveInput.normalized();
+		myMoveInput = myMoveInput.normalized();
 	if(Input.is_action_just_pressed("Disconnect")):
 		print("Disconnecting from server.");
 		get_tree().set_network_peer(null);
 	return;
 
 func _physics_process(delta):
-	# Gravity
+	var compensatedMoveInput = myMoveInput;
+	if(!is_on_floor()):
+		myVelocity.y -= myGravity;
+		compensatedMoveInput *= 0.2;
+		
+	var rotatedInputVector = compensatedMoveInput.rotated(Vector3(0,1,0), get_node("PlayerCamera").rotation.y + rotation.y + 0.5 * PI);
+	var speedVector = rotatedInputVector * myAcceleration + myVelocity;
 	
-	var space_state = get_world().direct_space_state;
-	var result = space_state.intersect_ray(transform.origin + Vector3(0, 0.3, 0), transform.origin + Vector3(0, -1.0, 0));
-	if(!result.empty()):
-		if(myVelocity.y <= 0):
-			myMoveInput.y = myVelocity.y + (result.position.y - transform.origin.y);
-		else:
-			myMoveInput.y = result.position.y - transform.origin.y;
-		clamp(myVelocity.y, -0.4, 1000);
-		print(myMoveInput.y);
-	else:
-		myMoveInput.y = myVelocity.y - 0.4;
-		print("No results");
-
-#	var isOnGround : bool = true;
-#	var fallAccelleration = 0.0;
-#	if(!test_move(transform, Vector3(0, -0.01, 0))):
-#		isOnGround = false;
-#		fallAccelleration = 10;
-#	if(!isOnGround && !test_move(transform, Vector3(0, -0.10, 0))):
-#		fallAccelleration = 100;
-#	if(!isOnGround && !test_move(transform, Vector3(0, -0.40, 0))):
-#		fallAccelleration = 40;
-#	if(!isOnGround):
-#		myMoveInput.y = 0.0 if myVelocity.y > 0 else myVelocity.y - (fallAccelleration * delta);
+	speedVector.y = clamp(speedVector.y, -10, 1000);
+	var compensatedSpeedVector = Vector2(speedVector.x, speedVector.z).clamped(myMaxSpeed);
+	speedVector.x = compensatedSpeedVector.x;	
+	speedVector.z = compensatedSpeedVector.y;
+	speedVector.x /= myFriction; 
+	speedVector.z /= myFriction; 
+	
+	var up = Vector3(0,1,0);
+	myVelocity = move_and_slide(speedVector, up);
 	
 	# Send the server all the information we need!
-	var forwardMovement = myMoveInput.rotated(Vector3(0,1,0), get_node("PlayerCamera").rotation.y + rotation.y + 0.5 * PI);
-	var up = Vector3(0,1,0);
-	myVelocity = move_and_slide(forwardMovement, up);
-		
 	if(myNetworkEventHanlder.myIsConnected):
 		rpc_unreliable_id(1, "UpdatePlayerTransform", transform, myCamera.transform); 
 	
